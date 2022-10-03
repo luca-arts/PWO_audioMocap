f4a023cada6addb617607436e89b25b894021078
Kasper Jordaens <kasper.jordaens@gmail.com>
2022-10-03 14:52:02 +0200

    processdata gives correct midifiles

--- process_deeptrack.py
+++ process_deeptrack.py
@@ -2,10 +2,27 @@ import pickle
 from time import time,sleep
 import os
 import mido
+from mido import Message, MidiFile, MidiTrack, second2tick
+
+
+#### configuration options
+tpb = 480 ## resolution of MIDI file (ticks per beat)
+bpm = 120 ## beats per minute
+fps = 30 ## frames per second of video file VIDEO FILES CANNOT HAVE A VARIABLE FRAME RATE! if that is the case, reencode prior to processing
+loop = False ## keep repeating, only usefull for debugging and realtime
+realtime = False ## if true, send midi at video framerate, if False, writes file as fast as possible
+midifilename = 'posdata_track480rt.mid'
+# normalise data, input video dimensions here
+maxx = 3840
+maxy = 1920
+totalframes = 3529
 
 
 
 columns, lines = os.get_terminal_size()
+mid = MidiFile(ticks_per_beat=tpb)
+track = MidiTrack()
+mid.tracks.append(track)
 
 ##### helpers
 def calculateAngle(x, xsize, maxx):
@@ -20,12 +37,14 @@ def printNoFlush(text, x):
 
 # print(mido.get_output_names()) # To list the output ports
 outportlist = mido.get_output_names()
-print(outportlist)
+
+for i,dev in enumerate(outportlist):
+    print("port ", i, " ", dev)
 
 mididev = int(input("select mididevice from list (0-based index number) ")) 
 
 outport = mido.open_output(outportlist[mididev])
-
+print(outport, "selected")
 
 
 
@@ -58,6 +77,7 @@ loadit = input("do you want to load previous trackdata file? y/n :")
 if loadit == "y":
     trackdata = pickle.load(open('trackdata.pickle', 'rb'))
 else:
+    
     tracks = open('/home/kaos/Documents/LUCA/PWO/AudioMOCAP/Yolov5_DeepSort_Pytorch/runs/track/exp/tracks/20220919_144159_037.txt', 'r')
     frametracks = tracks.readlines()
     # trackdata =  {0:[{"id":0,"x":0,"y":0,"sx":0,"sy":0}]}  # frame : [list of tracked sound objects, top left x, y, sizex, sizey]
@@ -81,54 +101,77 @@ else:
 
     print(trackdata)
     pickle.dump(trackdata, open('trackdata.pickle', 'wb'))
-# normalise data, input video dimensions here
-maxx = 3840
-maxy = 1920
-fps = 30
-totalframes = 3473
+
 
 # print (trackdata)
 frames = list(trackdata.keys())
 # print(frames)
+# while loop:
 count=0
+prevcount = 0
 starttime = time()
 t1 = starttime
-while t1 < starttime + totalframes/fps:
-    t1 = time()
-    t2 = starttime + frames[count]/30
-    # if frames[count]%30 == 0:
-        # print("sec = " , int(frames[count]/30))
-    # print("----------")
-    # print("t1 = ", t1)
-    # print("t2 = ", t2)
-    # print("--------")
-    # other stuff here
-    # t2 = time.time()
-    # delta = t2 - t1
-    if t1 > t2:
-        # send midi
-        # print("frame = ", frames[count])
-        if len(trackdata[frames[count]]) > 0:
-            # if int(frames[count]/30) > 80:
-                # print(trackdata[frames[count]])
+miditime = 0
+
+miditick = 0
+lastmiditick = miditick
+# ticksperframe = 32 
+ticksperframe = int(((bpm/60)*tpb)/fps)
+print("ticksperframe = ", ticksperframe)
+
+if realtime:
+    while t1 < starttime + totalframes/fps:    
+        t1 = time()
+        t2 = starttime + frames[count]/fps
+        if t1 > t2:
+            if len(trackdata[frames[count]]) > 0:
+                delta = (frames[count] - frames[prevcount])
                 posstring = " "*columns
                 for i,tr in enumerate(trackdata[frames[count]]):
+                    miditick = int(delta  * ticksperframe)
+                    if i > 0:
+                        miditick = 0
                     x = tr['x']
                     xsize = tr['sx']
                     a = calculateAngle(x, xsize, maxx)
                     xpos = columns*(a+0.5)
                     xpos = int(xpos)
-                    # print(xpos)
                     posstring = posstring[:xpos] + str(tr['id']) + posstring[xpos+1:]
-                    outport.send(mido.Message('control_change', control=tr['id'], value=int((a+0.5)*127)))
-                # printNoFlush(" "*columns,0)
+                    outport.send(mido.Message('control_change', control=20+tr['id'], value=int((a+0.5)*127)))
+                    track.append(Message('control_change', control=20+tr['id'],  value=int((a+0.5)*127), time=miditick))
+                    lastmiditick = miditick
                 printNoFlush(posstring,0)
-            # pass
+                prevcount = count
+            count += 1
+        else:
+            sleep(0.02)
+else:
+    frame = 0
+    while frame < totalframes-1:
+        # print("\n",frame," ",count,"\n")
+        if len(trackdata[frames[count]]) > 0:
+            delta = frames[count] - frames[prevcount]
+            posstring = " "*columns  
+            for i,tr in enumerate(trackdata[frames[count]]):
+                miditick = int(delta  * ticksperframe)
+                if i > 0:
+                    miditick = 0
+                    # print("\n",frame," ",miditick,"\n")
+                x = tr['x']
+                xsize = tr['sx']
+                a = calculateAngle(x, xsize, maxx)
+                xpos = columns*(a+0.5)
+                xpos = int(xpos)
+                posstring = posstring[:xpos] + str(tr['id']) + posstring[xpos+1:]
+                outport.send(mido.Message('control_change', control=20+tr['id'], value=int((a+0.5)*127)))
+                track.append(Message('control_change', control=20+tr['id'],  value=int((a+0.5)*127), time=miditick))
+                # lastmiditick = miditick
+            printNoFlush(posstring,0)
+            prevcount = count
         count += 1
-        # do stuff
-    else:
-        # sleep for a bit
-        sleep(0.02)
+        frame = frames[count]
 
+
+mid.save(midifilename)
 
 
